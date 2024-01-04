34574530cbb318b6f68b08d62e98cb8424a82fc7
amresh-web <amresh.web@gmail.com>
2024-01-05 05:16:16 +0530

    category page design

--- src/pages/smart-phone/SmartPhones.js
+++ src/pages/smart-phone/SmartPhones.js
@@ -1,7 +1,11 @@
+import cate from "../../assets/scss/Category.module.scss";
+import Banner from "../../components/Common/Banner";
+
 const SmartPhones = () => {
   return (
     <>
       <h1>Smart Phone</h1>
+      <Banner />
     </>
   );
 };
